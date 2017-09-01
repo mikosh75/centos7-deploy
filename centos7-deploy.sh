29dc08be123866ba5f3635d69c06cd4f58da1553
mikosh75 <mikosh@gmail.com>
2017-09-01 16:03:46 +0200

    Update centos7-deploy.sh

--- centos7-deploy.sh
+++ centos7-deploy.sh
@@ -8,7 +8,7 @@ curl -O http://mirror.switch.ch/ftp/mirror/epel/epel-release-latest-7.noarch.rpm
 curl -O https://repo.zabbix.com/zabbix/3.0/rhel/7/x86_64/zabbix-release-3.0-1.el7.noarch.rpm
 yum -y localinstall epel-release-latest-7.noarch.rpm zabbix-release-3.0-1.el7.noarch.rpm
 yum -y update
-yum -y install open-vm-tools htop vim zabbix-agent
+yum -y install open-vm-tools htop vim zabbix-agent net-tools
 systemctl start vmtoolsd
 systemctl enable vmtoolsd
 mkdir ~/.ssh
